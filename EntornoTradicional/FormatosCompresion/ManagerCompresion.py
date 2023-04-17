61e6f0b0ac0795771bb6167ea8b8299e2a330328
lwilches <lwilches@segurosmundal.com.co>
2023-04-16 20:22:32 -0500

    ajuste nombre clase  manager compresion

--- EntornoTradicional/FormatosCompresion/ManagerCompresion.py
+++ EntornoTradicional/FormatosCompresion/ManagerCompresion.py
@@ -8,8 +8,8 @@ class  ManagerCompresion( ):
     def comprimir(self, input_file) -> string :
         path_sin_extencion = os.path.splitext(input_file )[0]
         output_file =  f"{path_sin_extencion}.{self.adapter.dar_extension()}"
-        self.adapter.compress(input_file, output_file)
+        self.adapter.comprimir(input_file, output_file)
         return output_file
 
     def descomprimir(self, input_file, output_path):
-        self.adapter.decompress(input_file, output_path)
+        self.adapter.descomprimir(input_file, output_path)
--- EntornoTradicional/worker/tasks.py
+++ EntornoTradicional/worker/tasks.py
@@ -11,6 +11,8 @@ import ManagerCompresion
 import os 
  
 
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
