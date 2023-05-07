131ca2b8adb700c1bc72d2e7b035675df7e9f52f
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-05-06 19:14:47 -0600

    Eliminar sudo appnube.sh

--- appnube.sh
+++ appnube.sh
@@ -6,8 +6,8 @@ cd /root
 cd ClaseAppNube
 ​
 # Levantar la aplicación de Flask
-sudo export FLASK_APP=app.py
-sudo export  SQLALCHEMY_DATABASE_URI="postgresql://postgres:convert@35.239.90.117:5432/appnube"
-sudo export REDIS_URL="redis://10.128.0.5:6379/0"
-sudo export bucket_files="web_api_data"
+export FLASK_APP=app.py
+export  SQLALCHEMY_DATABASE_URI="postgresql://postgres:convert@35.239.90.117:5432/appnube"
+export REDIS_URL="redis://10.128.0.5:6379/0"
+export bucket_files="web_api_data"
 flask run
