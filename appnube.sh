938261d791f130b71546d7aa46938de8f604e72a
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-05-06 22:23:53 -0600

    cambios en appnube.sh

--- appnube.sh
+++ appnube.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
+cd /home/mr_gomezc1/ClaseAppNube/EntornoTradicional
 # Levantar la aplicación de Flask
 export FLASK_APP=app.py
-export SQLALCHEMY_DATABASE_URI="postgresql://postgres:convert@35.239.90.117:5432/appnube"
-export REDIS_URL="redis://10.128.0.5:6379/0"
-export bucket_files="web_api_data"
-python3 app.py
-
-
-
-
-
-
+python3 app.py   
