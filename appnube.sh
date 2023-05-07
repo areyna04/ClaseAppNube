e8c0effe7b661b37706b5b5276ca3b66354def93
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-05-06 19:14:07 -0600

    cambios a appnube.sh

--- appnube.sh
+++ appnube.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 ​
 cd /root
-# Verificar si Python está instalado y, si no, instalarlo
-if ! command -v python &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install python -y
-fi
-​
-# Verificar si pip está instalado y, si no, instalarlo
-if ! command -v pip &> /dev/null; then
-    sudo apt-get install python-pip -y
-fi
-​
-# Verificar si git está instalado y, si no, instalarlo
-if ! command -v git &> /dev/null; then
-    sudo apt-get install git -y
-fi
-​
-# Verificar si la carpeta existe y eliminarla si es necesario para traer version actual
-if [ -d "repositorio" ]; then
-    rm -rf ClaseAppNube
-fi
-​
-# Clonar el repositorio de GitHub
-git clone https://github.com/areyna04/ClaseAppNube
-​
+
 # Moverse al directorio del repositorio
 cd ClaseAppNube
 ​
-# Instalar los requisitos de Python
-pip install -r requirements.txt
-​
 # Levantar la aplicación de Flask
-export FLASK_APP=app.py
+sudo export FLASK_APP=app.py
+sudo export  SQLALCHEMY_DATABASE_URI="postgresql://postgres:convert@35.239.90.117:5432/appnube"
+sudo export REDIS_URL="redis://10.128.0.5:6379/0"
+sudo export bucket_files="web_api_data"
 flask run
