0fb1f00b8fc4a66efe9d63c80b2e33df72cd4ea0
Mario Gomez <mariorobingomez@gmail.com>
2023-05-13 21:35:13 -0500

    organizar Ambiente VMs GCP

--- EntornoTradicional/tasks.py
+++ EntornoTradicional/tasks.py
@@ -1,6 +1,7 @@
 
 from celery import Celery
 from worker.WorkerCompresion import TareaCompresion
+import os
 
 cnstringRedis =      os.environ.get("REDIS_URL") # "redis://localhost:6379/0" # os.environ["REDIS_URL"]
 app = Celery( 'tasks' , broker = cnstringRedis )
