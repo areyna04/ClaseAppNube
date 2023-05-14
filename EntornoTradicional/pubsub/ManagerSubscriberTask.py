7707666bb684e5321a10e67f6c4e4b39e334b942
lwilches <lwilches@segurosmundial.com.co>
2023-05-13 23:58:25 -0500

    ajustes suscriptores

--- EntornoTradicional/pubsub/ManagerSubscriberTask.py
+++ EntornoTradicional/pubsub/ManagerSubscriberTask.py
@@ -5,29 +5,33 @@ from worker.WorkerCompresion import TareaCompresion
 
 class ManagerSubscriberTask():
     
-    def __init__(self) -> None:
+    def __init__(self, project_id, topic_name) -> None:
         self.subscriber = pubsub_v1.SubscriberClient()
+        self.subscription_path = self.subscriber.subscription_path(project_id, f"{topic_name}-sub")
+        self.subscriber.create_subscription(name=self.subscription_path, topic=topic_name)
+        print(f'subscripcion  path {self.subscription_path}  topico : {topic_name} ' )
 
-
-    def suscribir_topic_comprimir(self, project_id, topic_name):
+    def suscribir_topic_comprimir(self):
         # Formatear el nombre completo del tema
-        subscription_path = self.subscriber.subscription_path(project_id, f"{topic_name}-sub")
+        try:
 
-        def comprimir_callback (mensaje):
-            tarea =  TareaCompresion()
-            id_request = mensaje.data.decode('utf-8')
-            resultado_ok= tarea.comprimir(id_request)
-            if resultado_ok :
-                mensaje.ack()
+            def comprimir_callback (mensaje):    
+                tarea =  TareaCompresion()
+                id_request = mensaje.data.decode('utf-8')
+                print(f'procesando mensaje   {id_request} ' )
+                resultado_ok= tarea.comprimir(id_request)
+                if resultado_ok :
+                    mensaje.ack()
 
-             
+                
+            # Iniciar la suscripción
+            streaming_pull_future = self.subscriber.subscribe(self.subscription_path, callback=comprimir_callback)
+            print(f'suscrito a subscripcion     {self.subscription_path} ' )
 
-
-        # Iniciar la suscripción
-        streaming_pull_future = self.subscriber.subscribe(subscription_path, callback=comprimir_callback)
-
-        # Mantener el proceso en ejecución mientras espera mensajes
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.submit(streaming_pull_future.result)    
-
+            # Mantener el proceso en ejecución mientras espera mensajes
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                print(f'corriendo worker')
+                executor.submit(streaming_pull_future.result)    
+        except KeyboardInterrupt:
+            streaming_pull_future.cancel()
 
--- EntornoTradicional/tasksSubscriber.py
+++ EntornoTradicional/tasksSubscriber.py
@@ -5,6 +5,6 @@ import os
 project_id =  os.getenv('GOOGLE_CLOUD_PROJECT')  
 topic_name =  os.environ.get("TOPIC")
 
-managerSub = ManagerSubscriberTask()
+managerSub = ManagerSubscriberTask(project_id,  topic_name)
 
-managerSub.suscribir_topic_comprimir(project_id,  topic_name)
+managerSub.suscribir_topic_comprimir()
