88c8b5292241e74023d2ee83b09a66569cdd9288
lwilches <lwilches@segurosmundial.com.co>
2023-05-24 00:07:07 -0500

    <fix> ajuste definiiones serviciones app engine

--- EntornoTradicional/views/viewWebHook.py
+++ EntornoTradicional/views/viewWebHook.py
@@ -11,17 +11,19 @@ import json
 class VistaWebHook(Resource):
 
     def post(self):
-       mensaje = json.loads(request.json)
-       ok  = self.comprimir_callback(mensaje)
+       mensaje =  request.get_json()
+       ok  = self.comprimir_callback(mensaje["message"]["data"])
        if ok :
            return 200
        else: 
-           return f"Error comprimiendo archivo {mensaje.data} ", 500
+           return f"Error comprimiendo archivo {mensaje} ", 500
 
 
     def comprimir_callback (self, mensaje):    
         tarea =  TareaCompresion()
-        id_request = mensaje.data.decode('utf-8')
+        binary_request  = base64.b64decode(mensaje)   ##mensaje.data.decode('utf-8')
+        id_request = binary_request.decode().strip()
+
         print(f'procesando mensaje   {id_request} ' )
 
         resultado_ok= tarea.comprimir(id_request)
